4d131e1a4564f485bbdd1750eb66d67cd2c5360e
FrontEnd <C:\Users\Work\AppData\Roaming\The Bat!\Google>
2023-08-11 00:01:13 +0300

    создаёт пример листа задач

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import TodoDetails from "./TodoDetails";
+import "./Main.css";
+import { motion } from "framer-motion";
+
+function App() {
+  return (
+    <div className="App">
+      <motion.h1
+        initial={{ y: -200 }}
+        animate={{ y: 0 }}
+        transition={{ type: "spring", duration: 0.5 }}
+        whileHover={{ scale: 1.1 }}
+      >
+        Todo App
+      </motion.h1>
+      <motion.div
+        initial={{ y: 1000 }}
+        animate={{ y: 0 }}
+        transition={{ type: "spring", duration: 1 }}
+      >
+        <TodoDetails />
+      </motion.div>
+    </div>
+  );
+}
+
+export default App;
